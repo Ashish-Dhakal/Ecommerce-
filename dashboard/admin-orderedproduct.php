51d475b10b215a7c05ad756674fdc4fd41c0488a
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-08 22:50:10 +0545

    prodesses and delete button work

--- dashboard/admin-orderedproduct.php
+++ dashboard/admin-orderedproduct.php
@@ -1,22 +1,83 @@
- <?php include '../dashboard/admin-dash.php'; ?>
+<?php include '../dashboard/admin-dash.php'; ?>
 
 <?php
+if (isset($_POST['process'])) {
+    $transactionId = $_POST['process'];
+
+    // Retrieve the relevant information based on the selected transaction ID
+    $query = "SELECT
+                c.c_fname,
+                c.c_lname,
+                GROUP_CONCAT(DISTINCT p.p_id ORDER BY p.p_id) AS product_ids,
+                py.p_amount AS total_amount,
+                py.transaction_id
+            FROM
+                customer c
+                LEFT JOIN payment py ON c.c_id = py.c_id
+                LEFT JOIN cart ca ON c.c_id = ca.c_id
+                LEFT JOIN product p ON ca.p_id = p.p_id
+            WHERE
+                py.transaction_id = '$transactionId'";
+
+    $result = $conn->query($query);
+    $row = $result->fetch_assoc();
+
+    // Store the retrieved information in the ordered table or perform any other actions as required
+
+    // Example: Storing the information in the ordered table
+    $fname = $row['c_fname'];
+    $lname = $row['c_lname'];
+    $productIds = $row['product_ids'];
+    $totalAmount = $row['total_amount'];
+    $transactionId = $row['transaction_id'];
+
+    // Perform the necessary database insert operation to store the information in the ordered table
+    $insertQuery = "INSERT INTO ordered (product_ids, total_amount, transaction_id)
+                    VALUES ('$productIds', '$totalAmount', '$transactionId')";
+                  
+    if ($conn->query($insertQuery)) {
+        // Information stored successfully
+        // Perform any additional actions or display a success message
+    } else {
+        // Error storing the information
+        echo "Error: Failed to store the information in the ordered table.";
+        die();
+    }
+}
+
+if (isset($_POST['delete'])) {
+    $transactionId = $_POST['delete'];
+
+    // Perform the necessary database delete operation to remove the information from the payment table
+    $deleteQuery = "DELETE FROM payment WHERE transaction_id = '$transactionId'";
+
+    if ($conn->query($deleteQuery)) {
+        // Information deleted successfully
+        // Perform any additional actions or display a success message
+    } else {
+        // Error deleting the information
+        echo "Error: Failed to delete the information from the payment table.";
+        die();
+    }
+}
+
+// Retrieve the updated list of transactions for display
 $query = "SELECT
-  c.c_fname,
-  c.c_lname,
-  GROUP_CONCAT(DISTINCT p.p_id ORDER BY p.p_id) AS product_ids,
-  py.p_amount AS total_amount,
-  py.transaction_id
-FROM
-  customer c
-  LEFT JOIN payment py ON c.c_id = py.c_id
-  LEFT JOIN cart ca ON c.c_id = ca.c_id
-  LEFT JOIN product p ON ca.p_id = p.p_id
-WHERE
-  py.transaction_id IS NOT NULL
-GROUP BY
-  c.c_id, py.transaction_id;
-";
+            c.c_fname,
+            c.c_lname,
+            GROUP_CONCAT(DISTINCT p.p_id ORDER BY p.p_id) AS product_ids,
+            py.p_amount AS total_amount,
+            py.transaction_id
+        FROM
+            customer c
+            LEFT JOIN payment py ON c.c_id = py.c_id
+            LEFT JOIN cart ca ON c.c_id = ca.c_id
+            LEFT JOIN product p ON ca.p_id = p.p_id
+        WHERE
+            py.transaction_id IS NOT NULL
+        GROUP BY
+            c.c_id, py.transaction_id";
+
 $result = $conn->query($query);
 ?>
 
@@ -52,4 +113,4 @@ $result = $conn->query($query);
             <?php } ?>
         </tbody>
     </table>
-</main> 
+</main>
