9e45d7a3b43e492960627906fa9746d745733bf1
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-08 22:45:40 +0545

    displaying the product from the payment table

--- dashboard/admin-orderedproduct.php
+++ dashboard/admin-orderedproduct.php
@@ -1,60 +1,55 @@
- <?php include '../dashboard/admin-dash.php';
+ <?php include '../dashboard/admin-dash.php'; ?>
 
-    ?>
+<?php
+$query = "SELECT
+  c.c_fname,
+  c.c_lname,
+  GROUP_CONCAT(DISTINCT p.p_id ORDER BY p.p_id) AS product_ids,
+  py.p_amount AS total_amount,
+  py.transaction_id
+FROM
+  customer c
+  LEFT JOIN payment py ON c.c_id = py.c_id
+  LEFT JOIN cart ca ON c.c_id = ca.c_id
+  LEFT JOIN product p ON ca.p_id = p.p_id
+WHERE
+  py.transaction_id IS NOT NULL
+GROUP BY
+  c.c_id, py.transaction_id;
+";
+$result = $conn->query($query);
+?>
 
- <?php
-    $query = "SELECT c.c_id ,c.c_fname, c.c_lname, p.p_amount, p.transaction_id
-    FROM customer AS c
-    JOIN payment AS p ON c.c_id = p.c_id;";
-    $result = $conn->query($query);
-    $row = $result->fetch_assoc();
-
-
-    $c_id = $row['c_id'];
-    $c_fname = $row['c_fname'];
-    $c_lname = $row['c_lname'];
-    $p_amount = $row['p_amount'];
-    $transaction_id = $row['transaction_id'];
-
-
-    ?>
- <main class="main-container">
-     <?php //show($_SESSION); ?>
-
-
-     <table class="table table-striped" style="text-align: center;">
-         <thead>
-             <tr>
-                 <th scope="col">First Name</th>
-                 <th scope="col">Last Name</th>
-                 <th scope="col">Total Product</th>
-                 <th scope="col">Total Price</th>
-                 <th scope="col">Transaction ID</th>
-                 <th scope="col" colspan="2">Action</th>
-             </tr>
-         </thead>
-         <tbody>
-             <tr>
-                 <td><?php echo $c_fname
-                        ?></td>
-                 <td><?php echo $c_lname
-                        ?></td>
-                 <td><?php 
-                        ?></td>
-                 <td><?php echo $p_amount;
-                        ?></td>
-                 <td><?php echo $transaction_id;
-                        ?></td>
-                 <form action="" method="post">
-                     <td><button type="submit" name="checkout" value="<?php ?>" class="btn btn-success">Process</button>
-                     </td>
-                     <td><button type="submit" name="checkout" value="<?php ?>" class="btn btn-danger">Delete</button>
-                     </td>
-                 </form>
-
-
-             </tr>
-         </tbody>
-     </table>
-
- </main>
+<main class="main-container">
+    <table class="table table-striped" style="text-align: center;">
+        <thead>
+            <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Product IDs</th>
+                <th scope="col">Total Amount</th>
+                <th scope="col">Transaction ID</th>
+                <th scope="col" colspan="2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = $result->fetch_assoc()) { ?>
+                <tr>
+                    <td><?php echo $row['c_fname']; ?></td>
+                    <td><?php echo $row['c_lname']; ?></td>
+                    <td><?php echo $row['product_ids']; ?></td>
+                    <td><?php echo $row['total_amount']; ?></td>
+                    <td><?php echo $row['transaction_id']; ?></td>
+                    <form action="" method="post">
+                        <td>
+                            <button type="submit" name="process" value="<?php echo $row['transaction_id']; ?>" class="btn btn-success">Process</button>
+                        </td>
+                        <td>
+                            <button type="submit" name="delete" value="<?php echo $row['transaction_id']; ?>" class="btn btn-danger">Delete</button>
+                        </td>
+                    </form>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</main> 
