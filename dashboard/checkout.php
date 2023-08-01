439975a97e42b34e857f8e516e95d9c4192204ca
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-08-01 23:09:31 +0545

    payment sucessfully done

--- dashboard/checkout.php
+++ dashboard/checkout.php
@@ -1,102 +1,95 @@
  <?php include '../dashboard/user-dash.php'; ?>
  <main class="main-container">
- 
-         <?php
-        //  $q1 = "SELECT * FROM customer;";
-        //  $result1 = $conn->query($q1);
-        // $row1 = $result1->fetch_assoc();
-        // $c_id = $row1['c_id'];
 
-        // echo $c_id;
+     <h3>Process to checkout</h3>
 
+     <table class="table table-striped  ">
+         <tr>
+             <thead>
 
-        // Retrieve the customer's cart items
-        $c_fname = $_SESSION['c_fname'];
-        $query = "SELECT ct.cart_id , c.c_id 
-          FROM customer AS c
-          JOIN cart AS ct ON c.c_id = ct.c_id
-          WHERE c.c_fname = '$c_fname'";
-
-        $result = $conn->query($query);
-
-        // Store the cart_id values in an array
-        
-        $cartIds = array();
-        while ($row = $result->fetch_assoc()) {
-            $cartIds[] = $row['cart_id'];
-            $c_id = $row['c_id'];
-
-        }
-
-        // Check if transaction ID is submitted
-        if (isset($_POST['transaction_id'])!=null && !empty($cartIds)) {
-            $transaction_id = $_POST['transaction_id'];
-            $totalPrice = $_SESSION['totalPrice'];
+                 <th>Product Name</th>
+                 <th>Product Price</th>
+                 <th>Total Price</th>
+         </tr>
+         </thead>
+         <tbody>
+             <?php
+                $invoice_no =  $_GET['invoice_no'];
+               
+                $query = "SELECT orders_id,product_ids,total,invoice_no, status FROM orders   WHERE invoice_no = '$invoice_no' ";
+                // show($query);
+                $result = $conn->query($query);
+                // show($result);
                 
-
-            // Store payment details for each cart item
-            foreach ($cartIds as $cartId) {
-                $query = "INSERT INTO payment (c_id,p_amount, cart_id, transaction_id)
-                        VALUES ('$c_id','$totalPrice', '$cartId', '$transaction_id')";
-
-                if ($conn->query($query)) {
-                    // Perform any additional actions or validations if required
-                } else {
-                    echo "Error: Failed to store payment details.";
-                    die();
-                }
-            }
-
-            // Redirect the user to a success page or perform any necessary actions
-            header("Location: ./usercart.php");
-            exit();
-        } else {
-            // Redirect the user back to usercart.php if transaction ID is not entered or there are no cart items
-              echo "error add bhayena haoi  not added";
-            //header("Location: ./usercart.php");
-            exit();
-        }
-
-            ?>
+                
+                
+                // $row = $result->fetch_assoc();
+                // $total = $row['total'];
+                // while (mysqli_num_rows($row) == 1) {
+                //     show($row);
 
 
+                    if (mysqli_num_rows($result) > 0) {
+                        // Fetch the first row as an associative array
+                        $row = mysqli_fetch_assoc($result);
+                        $total = $row['total'];
+                ?>
+                 <tr>
+
+                     <th><?php echo $row['orders_id'] ?></th>
+                     <td><?php echo $row['product_ids'] ?></td>
+                     <td><?php echo $row['total'] ?></td>
+                 </tr>
+             <?php } ?>
+             <tr>
+                 <td></td>
+                 <th>Total <br>
+                 </th>
+                 <td><?php // echo $row['total'];
+                        // $_SESSION['totalPrice'] = $totalPrice; ?><br>
+                     <!-- <button type="submit" name="checkout" value="<?php ?>" class="btn btn-success">Checkout</button> -->
+                 </td>
+             </tr>
+             <tr>
+                 <td></td>
+                 <th>Pay vai </th>
+                 <th>
+
+                     <?php
+                        if (mysqli_num_rows($result) >= 1) {
+                        ?>
+                         <!-- <form method="post" action="./user-checkout.php"> -->
+                         <input type="hidden" name="c_fname" value=" <?php $_SESSION['c_fname']; ?>" placeholder="Enter transaction ID of payment">
 
 
-<!-- 
-          <table class="table table-striped" style="text-align: center;">
-            <thead>
-                <tr>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Total Product</th>
-                    <th scope="col">Total Price</th>
-                    <th scope="col">Transaction ID</th>
-                    <th scope="col" colspan="2">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?php // echo $_SESSION['c_fname']; 
-                        ?></td>
-                    <td><?php // echo $_SESSION['c_lname']; 
-                        ?></td>
-                    <td><?php //echo $_SESSION['rowCount']; 
-                        ?></td>
-                    <td><?php //echo $_SESSION['totalPrice']; 
-                        ?></td>
-                    <td><?php //echo $transaction_id; 
-                        ?></td>
-                    <form action="" method="post">
-                        <td><button type="submit" name="checkout" value="<?php ?>" class="btn btn-success">Process</button>
-                        </td>
-                        <td><button type="submit" name="checkout" value="<?php ?>" class="btn btn-danger">Delete</button>
-                        </td>
-                    </form>
 
 
-                </tr>
-            </tbody>
-        </table>  -->
+                         <!-- esewa ko form -->
+                         <form action="https://uat.esewa.com.np/epay/main" method="POST">
+                             <input value="<?php echo $total; ?>" name="tAmt" type="hidden">
+                             <input value="<?php echo $total; ?>" name="amt" type="hidden">
+                             <input value="0" name="txAmt" type="hidden">
+                             <input value="0" name="psc" type="hidden">
+                             <input value="0" name="pdc" type="hidden">
+                             <input value="EPAYTEST" name="scd" type="hidden">
+                             <input value="<?= $invoice_no ?>" name="pid" type="hidden">
+                             <input value="http://localhost/ecommerc/dashboard/esewa_payment_success.php?q=su" type="hidden" name="su">
+                             <input value="http://localhost/ecommerc/dashboard/esewa_payment_failed.php?q=fu" type="hidden" name="fu">
+                             <input class="checkout-button" type="image" src="../resources/image/esewa-logo.png" style=" height: 30px;">
+                         </form>
+                         <!-- esewa ko form close -->
+
+
+                         <!-- </form> -->
+                     <?php } else {
+                            echo "Add product to cart to purchase the product";
+                        }
+                        ?>
+                 </th>
+             </tr>
+         </tbody>
+
+     </table>
 
  </main>
  </body>
