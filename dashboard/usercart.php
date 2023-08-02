15d19b487120304c3b115da0be09e244c3505076
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-08-02 20:49:02 +0545

    unique invoice no and ordered button edited

--- dashboard/usercart.php
+++ dashboard/usercart.php
@@ -9,19 +9,19 @@
               JOIN product AS p ON ct.p_id = p.p_id 
               WHERE c.c_fname = '$c_fname' AND c.c_lname = '$c_lname'";
 
-
-
     $result = $conn->query($query); // Execute the query and assign the result
-
-
-    $rowCount = $result->num_rows;
-    $_SESSION['rowCount'] = $rowCount;
+    
+    //echo $cartId;
+    // $rowCount = $result->num_rows;
+    // $_SESSION['rowCount'] = $rowCount;
+    // echo  $_SESSION['rowCount'];
 
     if (isset($_POST['remove'])) {
         $cartId = intval($_POST['remove']);
+        echo $cartId;
 
         // Delete related payment rows first
-        $deletePaymentQuery = "DELETE FROM payment WHERE cart_id = $cartId";
+        $deletePaymentQuery = "DELETE FROM cart WHERE cart_id = $cartId";
         if ($conn->query($deletePaymentQuery)) {
             // Proceed with deleting the cart row
             $deleteCartQuery = "DELETE FROM cart WHERE cart_id = $cartId";
@@ -63,7 +63,12 @@
     }
 
     $product_ids_string = implode('-', $product_ids);
-    $invoice_no =$product_ids_string . date("-Y-m-d");
+    $_SESSION['product_ids_string'] = $product_ids_string;
+    // $invoice_no =md5($product_ids_string . date("-Y-m-d")."-".time(), 0, 8);
+
+    $invoice_no = substr(md5($product_ids_string . date("Y-m-d") . "-" . time()), 0, 10);
+
+
 
 
     ?>
@@ -81,11 +86,13 @@
             </tr>
         </thead>
         <tbody>
+
             <?php
             $result->data_seek(0); // Reset the result set pointer
             while ($row = $result->fetch_assoc()) {
                 // $invoice_no = $row['p_id'];
                 //   echo $invoice_no . date("-Y-m-d ");
+                //echo $customer_id;
 
 
             ?>
@@ -96,6 +103,7 @@
                     <td><?php echo $row['quantity'] ?></td>
                     <td><?php echo $row['total_price'] ?></td>
                     <td>
+
                         <form method="post" action="./usercart.php">
                             <button type="submit" name="remove" value="<?php echo $row['cart_id'] ?>" class="btn btn-danger">Remove</button>
                         </form>
@@ -108,14 +116,33 @@
                 <td></td>
                 <th><?php echo $totalPrice;
                     $_SESSION['totalPrice'] =  $totalPrice; ?></th>
-                <td>
-                    <form method="post" action="./user-update.php" method="post">
-                        <input type="hidden" value="<?php echo $invoice_no; ?>" name="invoice_no">
-                        <input type="hidden" value="<?php echo $product_ids_string; ?>" name="product_ids">
-                        <input type="hidden" value="<?php echo $totalPrice; ?>" name="total">
-                        <button type="submit" name="place_order" value="" class="btn btn-success">Place Order</button>
-                    </form>
-                </td>
+                <?php
+
+                ?>
+
+                <th style="color: red;" >
+                    <?php
+                    $query6 = " SELECT COUNT(*) FROM cart WHERE c_id = $customer_id";
+                    $result6 = $conn->query($query6);
+                    // show($result6);
+                    // show($query6);
+                   if (mysqli_num_rows($result) >= 1) 
+                   {   ?>
+                        <form method="post" action="./user-update.php" method="post">
+                            <input type="hidden" value="<?php echo $invoice_no; ?>" name="invoice_no">
+                            <input type="hidden" value="<?php echo $product_ids_string; ?>" name="product_ids">
+                            <input type="hidden" value="<?php echo $totalPrice; ?>" name="total">
+                            
+                            <button type="submit" name="place_order" value="" class="btn btn-success">Place Order</button>
+                        </form>
+                    <?php }
+                    else{
+                       echo '<a href="../index.php">add product item<br> to place order</a>';
+                   }
+
+                    ?>
+
+                </th>
             </tr>
         </tbody>
     </table>
